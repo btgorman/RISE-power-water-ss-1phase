fixed 288 water demand pdf to average to 100.4%

data_water/supplemental/demands/annual/annual_water_demand_factors.py
@@ -33,7 +33,7 @@ def lev_pdf(x, mu, sigma):
 def logistic_pdf(x, location, scale):
 	return np.exp(-(x-location) / scale) / (scale * (1. + np.exp(-(x-location) / scale))**2)
 
-monthly_demand_scalar = {
+monthly_annual_demand_scalar = {
  1: 064., 
  2: 065., 
  3: 070., 
@@ -47,6 +47,36 @@ monthly_demand_scalar = {
  11: 080., 
  12: 074., 
 }
+
+monthly_seasonal_demand_scalar = {
+ 1: 0.,
+ 2: 0.,
+ 3: 0.,
+ 4: 0.,
+ 5: 0.,
+ 6: 0.,
+ 7: 0.,
+ 8: 0.,
+ 9: 0.,
+ 10: 0.,
+ 11: 0.,
+ 12: 0.,
+}
+
+monthly_summer_average = 0.01*(monthly_annual_demand_scalar[6] + monthly_annual_demand_scalar[7] + monthly_annual_demand_scalar[8]) / 3
+monthly_winter_average = 0.01*(monthly_annual_demand_scalar[1] + monthly_annual_demand_scalar[2] + monthly_annual_demand_scalar[12]) / 3
+monthly_spring_average = 0.01*(monthly_annual_demand_scalar[2] + monthly_annual_demand_scalar[4] + monthly_annual_demand_scalar[5]) / 3
+monthly_fall_average = 0.01*(monthly_annual_demand_scalar[9] + monthly_annual_demand_scalar[10] + monthly_annual_demand_scalar[11]) / 3
+
+for item in monthly_seasonal_demand_scalar:
+	if item == 6 or item == 7 or item == 8:
+		monthly_seasonal_demand_scalar[item] = monthly_annual_demand_scalar[item] / monthly_summer_average
+	if item == 12 or item == 1 or item == 2:
+		monthly_seasonal_demand_scalar[item] = monthly_annual_demand_scalar[item] / monthly_winter_average
+	if item == 3 or item == 4 or item == 5:
+		monthly_seasonal_demand_scalar[item] = monthly_annual_demand_scalar[item] / monthly_spring_average
+	if item == 9 or item == 10 or item == 11:
+		monthly_seasonal_demand_scalar[item] = monthly_annual_demand_scalar[item] / monthly_fall_average
 
 hourly_summer_demand_scalar = {
 1: 100., 
@@ -102,31 +132,58 @@ hourly_winter_demand_scalar = {
 24: 050., 
 }
 
+# hourly_equinox_demand_scalar = {
+# 1: 075., 
+# 2: 070., 
+# 3: 065., 
+# 4: 073., 
+# 5: 080., 
+# 6: 094., 
+# 7: 108., 
+# 8: 124., 
+# 9: 140., 
+# 10: 142., 
+# 11: 133., 
+# 12: 117., 
+# 13: 100., 
+# 14: 096., 
+# 15: 093., 
+# 16: 108., 
+# 17: 125., 
+# 18: 142., 
+# 19: 158., 
+# 20: 168., 
+# 21: 178., 
+# 22: 168., 
+# 23: 158., 
+# 24: 075., 
+# }
+
 hourly_equinox_demand_scalar = {
-1: 075., 
-2: 070., 
-3: 065., 
-4: 073., 
-5: 080., 
-6: 094., 
-7: 108., 
-8: 124., 
-9: 140., 
-10: 142., 
-11: 133., 
-12: 117., 
-13: 100., 
-14: 096., 
-15: 093., 
-16: 108., 
-17: 125., 
-18: 142., 
-19: 158., 
-20: 168., 
-21: 178., 
-22: 168., 
-23: 158., 
-24: 075., 
+1: 54.,
+2: 51.,
+3: 47.,
+4: 53.,
+5: 58.,
+6: 68.,
+7: 78.,
+8: 90.,
+9: 101.,
+10: 103.,
+11: 96.,
+12: 85.,
+13: 72.,
+14: 69.,
+15: 67.,
+16: 78.,
+17: 90.,
+18: 103.,
+19: 114.,
+20: 121.,
+21: 129.,
+22: 121.,
+23: 114.,
+24: 54.
 }
 
 hourly_demand_scalar= {
@@ -149,64 +206,68 @@ summer_hourly_demands = []
 winter_hourly_demands = []
 equinox_hourly_demands = []
 
-for month in range(1, len(monthly_demand_scalar)+1):
+for month in range(1, len(monthly_seasonal_demand_scalar)+1):
 	for hour in range(1, len(hourly_equinox_demand_scalar)+1):
 		if month == 1 or month == 2 or month == 12:
-			alpha = monthly_demand_scalar[month] * 0.01
+			alpha = monthly_seasonal_demand_scalar[month] * 0.01
 			beta = hourly_demand_scalar[month][hour] * 0.01
 			winter_hourly_demands.append(alpha*beta)
 			total_hourly_demands.append(alpha*beta)
 		elif month == 3 or month == 4 or month == 5 or month == 9 or month == 10 or month == 11:
-			alpha = monthly_demand_scalar[month] * 0.01
+			alpha = monthly_seasonal_demand_scalar[month] * 0.01
 			beta = hourly_demand_scalar[month][hour] * 0.01
 			equinox_hourly_demands.append(alpha*beta)
 			total_hourly_demands.append(alpha*beta)
 		else:
-			alpha = monthly_demand_scalar[month] * 0.01
+			alpha = monthly_seasonal_demand_scalar[month] * 0.01
 			beta = hourly_demand_scalar[month][hour] * 0.01
 			summer_hourly_demands.append(alpha*beta)
 			total_hourly_demands.append(alpha*beta)
 
+# annual_hourly_demands = np.array(total_hourly_demands)
+# np.savetxt('waterdemands288.csv', annual_hourly_demands, fmt='%.6f', delimiter=',', newline='\n')
+
 count, bins, ignored = plt.hist([winter_hourly_demands, summer_hourly_demands, equinox_hourly_demands], int(288/8), stacked=True, label=['winter', 'summer', 'spring/fall'])
 sumcount = sum(count[-1]) * math.fabs(bins[0] - bins[1])
 
+# x = np.arange(0.0, max(total_hourly_demands), 0.08)
 x = np.arange(0.0, max(total_hourly_demands) - min(total_hourly_demands), 0.08) # for exponential distribution
 
-# weib_shape = 1./0.66408412
+# weib_shape = 1./0.52599695
 # weib_scale = np.exp(0.34368128)
 # plt.plot(x, weibull_pdf(x, weib_scale, weib_shape) * sumcount, color="teal", label="weibull")
 
-# lnorm_mu = -0.0121681
-# lnorm_sigma = 0.71567165
+# lnorm_mu = -0.13181
+# lnorm_sigma = 0.5054088
 # plt.plot(x, lognormal_pdf(x, lnorm_mu, lnorm_sigma) * sumcount, label="lognormal")
 
-exp_rate = 1./np.exp(0.0144362)
+exp_rate = 1./np.exp(-0.4559995)
 plt.plot(x+min(total_hourly_demands), exponential_pdf(x, exp_rate) * sumcount, label="exponential")
 
-# frech_shape = 1./0.66505673
-# frech_scale = np.exp(-0.3700751)
+# frech_shape = 1./0.42136022
+# frech_scale = np.exp(-0.3719968)
 # frech_loc = 0.
 # plt.plot(x, frechet_pdf(x, frech_loc, frech_scale, frech_shape) * sumcount, label="frechet")
 
-# llog_shape = 1./0.42540507
-# llog_scale = np.exp(-0.0047049)
+# llog_shape = 1./0.29436175
+# llog_scale = np.exp(-0.1624727)
 # plt.plot(x, loglogistic_pdf(x, llog_scale, llog_shape) * sumcount, label="log-logistic")
 
-# sev_mu = 1.75932773
-# sev_sigma = 1.09390468
+# sev_mu = 1.2354205
+# sev_sigma = 0.6650083
 # plt.plot(x, sev_pdf(x, sev_mu, sev_sigma) * sumcount, label="sev")
 
-# norm_mu = 1.26349549
-# norm_sigma = 0.89910515
+# norm_mu = 1.00406644
+# norm_sigma = 0.57272733
 # plt.plot(x, normal_pdf(x, norm_mu, norm_sigma) * sumcount, label="normal")
 
-# lev_mu = 0.87361666
-# lev_sigma = 0.61594864
+# lev_mu = 0.76293197
+# lev_sigma = 0.37014512
 # plt.plot(x, lev_pdf(x, lev_mu, lev_sigma) * sumcount, color="red", label="lev")
 
-# log_location = 1.14021671
-# log_scale = 0.48387537
-# plt.plot(x, logistic_pdf(x, log_location, log_scale) * sumcount, label="logistic")
+# log_loc = 0.91335971
+# log_scale = 0.29913738
+# plt.plot(x, logistic_pdf(x, log_loc, log_scale) * sumcount, label="logistic")
 
 arialfont = {'fontname': 'Arial'}
 plt.xticks(fontsize="large", **arialfont)